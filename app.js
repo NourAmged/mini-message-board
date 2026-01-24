51467b53bfd1aefc7b76e38482d4930f4707d636
Nour Amged <nouramgged2005@gmail.com>
2026-01-24 15:03:41 +0200

    fixed small problem with nav bar and implemented form post request

--- app.js
+++ app.js
@@ -12,10 +12,12 @@ const assetsPath = path.join(__dirname, "public");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-app.use(express.static(assetsPath));
 
-app.use('/', indexRouter);
+app.use(express.static(assetsPath));
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/new', newMessageRouter);
+app.use('/', indexRouter);
 
 app.listen(PORT, () => {
     console.log(`app is listening on port ${PORT}`);
--- routes/newMessageRouter.js
+++ routes/newMessageRouter.js
@@ -1,8 +1,21 @@
 const { Router } = require("express");
+
 const newMessage = require("../controllers/newMessage");
+const messages = require("../messages");
 
 const newMessageRouter = Router();
 
+
 newMessageRouter.get("/", newMessage);
+
+newMessageRouter.post("/", (req, res) => {
+    messages.push({
+        text: req.body.message,
+        user: req.body.name,
+        added: new Date()
+    });
+
+    res.redirect("/");
+});
 
 module.exports = newMessageRouter;
