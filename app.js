49a741d04584f77330fa0f297ebda658ed2606ee
Nour Amged <nouramgged2005@gmail.com>
2026-01-24 13:30:03 +0200

    made the sub directories and create the index route

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("node:path");
+
+const indexRouter = require('./routes/indexRouter');
+
+const PORT = 3000;
+
+const app = express();
+
+const assetsPath = path.join(__dirname, "public");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.static(assetsPath));
+
+app.use('/', indexRouter);
+
+app.listen(PORT, () => {
+    console.log(`app is listening on port ${PORT}`);
+});
---
+++ routes/indexRouter.js
@@ -0,0 +1,8 @@
+const { Router } = require("express");
+const getMessages = require("../controllers/getMessages");
+
+const indexRouter = Router();
+
+indexRouter.get("/", getMessages);
+
+module.exports = indexRouter;
